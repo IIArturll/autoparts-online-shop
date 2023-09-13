517791be9a500d76df9840d874573ad7c583213f
psycho <moseyarthas@gmail.com>
2023-09-13 22:26:58 +0300

    add role and status fields to user entity

--- backend/user-service/src/main/java/com/autoparts/userservice/entity/UserEntity.java
+++ backend/user-service/src/main/java/com/autoparts/userservice/entity/UserEntity.java
@@ -21,10 +21,16 @@ public class UserEntity {
 
     private String phone;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
     public UserEntity() {
     }
 
-    public UserEntity(UUID id, String username, String password, String email, String firstname, String lastname, String phone) {
+    public UserEntity(UUID id, String username, String password, String email, String firstname, String lastname, String phone, Role role, Status status) {
         this.id = id;
         this.username = username;
         this.password = password;
@@ -32,6 +38,8 @@ public class UserEntity {
         this.firstname = firstname;
         this.lastname = lastname;
         this.phone = phone;
+        this.role = role;
+        this.status = status;
     }
 
     public UUID getId() {
@@ -88,5 +96,21 @@ public class UserEntity {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
     }
 }
