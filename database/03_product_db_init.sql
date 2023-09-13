f29cc9d1a5c57f9733d2b2493a526857295c8081
Artur <sharam.artur@gmail.com>
2023-09-13 23:40:12 +0300

    create tables for product and fill their

---
+++ database/03_product_db_init.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA IF NOT EXISTS  autoparts_shop;
+
+CREATE TABLE IF NOT EXISTS autoparts_shop.product_category
+(
+	id SMALLSERIAL NOT NULL,
+	category TEXT NOT NULL UNIQUE,
+	CONSTRAINT product_category_pkey PRIMARY KEY (id)	
+);
+
+CREATE TABLE IF NOT EXISTS autoparts_shop.car_brand
+(
+	id SMALLSERIAL NOT NULL,
+	brand TEXT NOT NULL UNIQUE,
+	CONSTRAINT car_brand_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS autoparts_shop.product
+(
+	id UUID NOT NULL,
+	title TEXT NOT NULL,
+	category SMALLINT NOT NULL,
+	car_brand SMALLINT NOT NULL,
+	description TEXT NOT NULL,
+	price DECIMAL NOT NULL,
+	amount INT NOT NULL,
+	CONSTRAINT uuid_product PRIMARY KEY (id),
+	FOREIGN KEY (category)
+		REFERENCES autoparts_shop.product_category (id),
+	FOREIGN KEY (car_brand)
+		REFERENCES autoparts_shop.car_brand (id)
+);
---
+++ database/04_product_db_fill.sql
@@ -0,0 +1,5 @@
+INSERT INTO autoparts_shop.product_category (category)
+VALUES ('engine'),('dashboard '),('lens'),('carburetor');
+
+INSERT INTO autoparts_shop.car_brand(brand)
+VALUES ('Mercedes'),('BMW'),('Toyota'),('Opel');
