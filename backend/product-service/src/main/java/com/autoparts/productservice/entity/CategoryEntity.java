2cad5708995215bce540ce27c5660cccbfdb6560
Artur <sharam.artur@gmail.com>
2023-09-14 00:14:32 +0300

    add entities for products

---
+++ backend/product-service/src/main/java/com/autoparts/productservice/entity/CategoryEntity.java
@@ -0,0 +1,34 @@
+package com.autoparts.productservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "autoparts_shop",name = "product_category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(generator = "category_seq", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "category_seq", sequenceName = "product_category_id_seq",
+            schema = "autoparts_shop", allocationSize = 1)
+    private Short id;
+    private String category;
+
+    public CategoryEntity() {
+    }
+
+    public CategoryEntity(String category){
+        this.category=category;
+    }
+
+    public Short getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
+
