a6459f1a91ddf7a931b2c2692fa402e413f5f5a7
Artur <sharam.artur@gmail.com>
2023-09-17 13:28:24 +0300

    add interfafaces and services for product, car brand and category

---
+++ backend/product-service/src/main/java/com/autoparts/productservice/services/api/IProductService.java
@@ -0,0 +1,16 @@
+package com.autoparts.productservice.services.api;
+
+import com.autoparts.productservice.core.ProductDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface IProductService {
+    ProductDTO find(UUID uuid);
+
+    Page<ProductDTO> getPage(Pageable pageable);
+
+    void add(ProductDTO product);
+}
