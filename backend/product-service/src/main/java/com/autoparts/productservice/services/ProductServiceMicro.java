910ed0c7f8c1cef21c9251f1f967d0e60953f06a
Artur <sharam.artur@gmail.com>
2023-10-03 00:56:09 +0300

    add service and contoller for micro-service discovery

---
+++ backend/product-service/src/main/java/com/autoparts/productservice/services/ProductServiceMicro.java
@@ -0,0 +1,27 @@
+package com.autoparts.productservice.services;
+
+import com.autoparts.productservice.core.ProductDTO;
+import com.autoparts.productservice.core.ProductMapper;
+import com.autoparts.productservice.entity.ProductEntity;
+import com.autoparts.productservice.repositories.IProductRepository;
+import com.autoparts.productservice.services.api.IProductServiceMicro;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Transactional
+public class ProductServiceMicro implements IProductServiceMicro {
+    private final IProductRepository repository;
+
+    public ProductServiceMicro(IProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public ProductEntity find(UUID uuid) {
+        return repository.findById(uuid).orElseThrow(() ->
+                new RuntimeException("there is no product with this uuid"));
+    }
+}
