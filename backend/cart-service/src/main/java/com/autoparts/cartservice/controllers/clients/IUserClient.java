77e3d688b1f69a713810fac545f7d37e601ed866
Artur <sharam.artur@gmail.com>
2023-10-27 23:24:57 +0300

    redone product feign client and add user feign client

--- backend/cart-service/src/main/java/com/autoparts/cartservice/controllers/clients/IUserClient.java
+++ backend/cart-service/src/main/java/com/autoparts/cartservice/controllers/clients/IUserClient.java
@@ -1,6 +1,6 @@
 package com.autoparts.cartservice.controllers.clients;
 
-import com.autoparts.cartservice.entity.product.ProductEntity;
+import com.autoparts.cartservice.entity.user.UserEntity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Optional;
 import java.util.UUID;
 
-@FeignClient(value = "product-service",path = "micro/product")
-public interface IProductClient {
+@FeignClient(value = "user-service", path = "micro/user")
+public interface IUserClient {
     @GetMapping("/{id}")
-    ResponseEntity<Optional<ProductEntity>> getCard(@PathVariable UUID id);
+    ResponseEntity<Optional<UserEntity>> get(@PathVariable("id") UUID id);
 }
