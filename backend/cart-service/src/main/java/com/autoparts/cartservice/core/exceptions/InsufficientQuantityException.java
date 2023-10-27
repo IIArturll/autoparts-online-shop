68fc9090d8ea15eb105dd8cfaa3f6436335483a7
Artur <sharam.artur@gmail.com>
2023-10-27 23:20:25 +0300

    add json ignore property for exception, add validation in AddProductDTO

--- backend/cart-service/src/main/java/com/autoparts/cartservice/core/exceptions/InsufficientQuantityException.java
+++ backend/cart-service/src/main/java/com/autoparts/cartservice/core/exceptions/InsufficientQuantityException.java
@@ -1,5 +1,8 @@
 package com.autoparts.cartservice.core.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = {"cause", "localizedMessage", "stackTrace", "suppressed"})
 public class InsufficientQuantityException extends RuntimeException {
     public InsufficientQuantityException(String message) {
         super(message);
