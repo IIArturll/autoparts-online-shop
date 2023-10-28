a0a27a2f59a71ed12f8319f89bb435d55e02e4f3
Artur <sharam.artur@gmail.com>
2023-10-28 20:49:48 +0300

    make find ingnore case for brand and category and check on exist

--- backend/product-service/src/main/java/com/autoparts/productservice/repositories/ICategoryRepository.java
+++ backend/product-service/src/main/java/com/autoparts/productservice/repositories/ICategoryRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface ICategoryRepository extends CrudRepository<CategoryEntity,Short> {
-    Optional<CategoryEntity> findByCategory(String category);
+    Optional<CategoryEntity> findByCategoryIgnoreCase(String category);
 }
--- backend/product-service/src/main/java/com/autoparts/productservice/services/CarBrandService.java
+++ backend/product-service/src/main/java/com/autoparts/productservice/services/CarBrandService.java
@@ -1,10 +1,13 @@
 package com.autoparts.productservice.services;
 
+import com.autoparts.productservice.core.exceptions.ResourceAlreadyExist;
 import com.autoparts.productservice.entity.CarBrandEntity;
 import com.autoparts.productservice.repositories.ICarBrandRepository;
 import com.autoparts.productservice.services.api.ICarBrandService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,12 +20,16 @@ public class CarBrandService implements ICarBrandService {
 
     @Override
     public CarBrandEntity find(String brand) {
-        return repository.findByBrand(brand).orElse(null);
+        return repository.findByBrandIgnoreCase(brand).orElse(null);
     }
 
     @Override
     public CarBrandEntity add(String brand) {
-        CarBrandEntity entity=new CarBrandEntity(brand);
+        Optional<CarBrandEntity> brandEntity = repository.findByBrandIgnoreCase(brand);
+        if (brandEntity.isPresent()) {
+            throw new ResourceAlreadyExist("Brand " + brand + " already exist");
+        }
+        CarBrandEntity entity = new CarBrandEntity(brand);
         repository.save(entity);
         return entity;
     }
