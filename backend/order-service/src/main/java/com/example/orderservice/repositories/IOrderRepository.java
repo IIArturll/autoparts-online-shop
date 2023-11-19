c823812b776bb9aea7e346415d4ae1cdecfd53eb
Artur <sharam.artur@gmail.com>
2023-11-19 03:09:43 +0300

    add methods in repository for get orders for idividual user by email and get order that not collected

--- backend/order-service/src/main/java/com/example/orderservice/repositories/IOrderRepository.java
+++ backend/order-service/src/main/java/com/example/orderservice/repositories/IOrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.orderservice.repositories;
 
 import com.example.orderservice.entity.OrderEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +12,8 @@ import java.util.UUID;
 @Repository
 public interface IOrderRepository extends CrudRepository<OrderEntity, UUID>,
         PagingAndSortingRepository<OrderEntity, UUID> {
+    Page<OrderEntity> findAllByUserEmailOrderByReady(String email, Pageable pageable);
+
+    Page<OrderEntity> findAllByReadyIsFalseOrderByOrderTimeDesc(Pageable pageable);
+
 }
