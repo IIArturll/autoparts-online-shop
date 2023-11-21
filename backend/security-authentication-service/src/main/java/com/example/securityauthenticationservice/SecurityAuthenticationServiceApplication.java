3383d248b67c47b2e54908143d00ed642c825dc1
Artur <sharam.artur@gmail.com>
2023-11-21 23:15:29 +0300

    init microservice for security and authentication

---
+++ backend/security-authentication-service/src/main/java/com/example/securityauthenticationservice/SecurityAuthenticationServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.securityauthenticationservice;
+
+import com.example.securityauthenticationservice.properties.JwtProperty;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableConfigurationProperties({JwtProperty.class})
+public class SecurityAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAuthenticationServiceApplication.class, args);
+    }
+}
