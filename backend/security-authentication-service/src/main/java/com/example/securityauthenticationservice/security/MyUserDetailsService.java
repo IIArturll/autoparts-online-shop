74a9d0b237a6db9718b4768acb1939fdba9c7da6
Artur <sharam.artur@gmail.com>
2023-11-21 23:18:16 +0300

    add custom user details, user detail service and add user feign client

---
+++ backend/security-authentication-service/src/main/java/com/example/securityauthenticationservice/security/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.securityauthenticationservice.security;
+
+import com.example.securityauthenticationservice.controllers.client.IUserClient;
+import com.example.securityauthenticationservice.core.dtos.MyUserDetails;
+import com.example.securityauthenticationservice.core.exceptions.ResourceNotFoundException;
+import com.example.securityauthenticationservice.core.mappers.UserMapper;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+    private final IUserClient client;
+
+    public MyUserDetailsService(IUserClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public MyUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return UserMapper.convertToUserDetails(client.getByEmail(email).getBody()
+                .orElseThrow(() -> new ResourceNotFoundException("user with this email not found")));
+    }
+}
